8a5844c0bfdda9a967e65f6889d676bb92ce3b79
Mohammad Assad <mohammadassad@Mohammads-MacBook-Pro.local>
2019-12-11 00:40:49 +0800

    added search functionality, search bar will search for users

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
      loading: false
    }
 
-   async componentDidMount(){
+  /* async componentDidMount(){
      
     this.setState({
       loading: true
@@ -25,6 +25,19 @@ class App extends Component {
       loading: false
     })
 
+  }*/
+
+  //search github users
+  searchUsers  = async text =>{
+
+    this.setState({loading:true})
+    console.log(text);
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({
+      users:res.data.items,
+      loading: false
+    })
   }
 
   render(){
@@ -38,7 +51,7 @@ class App extends Component {
       <div className="App">
        <Navbar/>
        <div className ="container">
-         <Search />
+         <Search searchUsers={this.searchUsers}/>
        <Users loading={this.state.loading} users= {this.state.users} />
        </div>
        
